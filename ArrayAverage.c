3c910c29d54e3b3af7c0089e59627f20308fd03f
KarloSiric <ksiric99@gmail.com>
2024-04-13 15:34:20 +0200

    commited

--- ArrayAverage.c
+++ ArrayAverage.c
@@ -2,15 +2,37 @@
 * @Author: karlosiric
 * @Date:   2024-04-13 00:37:55
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2024-04-13 00:42:01
+* @Last Modified time: 2024-04-13 15:33:55
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-
+    
 int main(void) {
     
+    double arr1[] = {5.2,4.9,3.2,6.0,7.1,6.5};
     
+    int len = sizeof(arr1) / sizeof(arr1[0]);
     
+    double sum = 0.0;
+    
+    for (int i = 0; i < len; ++i)
+    {   
+        sum += arr1[i];
+        printf("Adding %lf to the sum\n", arr1[i]);
+        if(i == len - 1)
+        {
+            printf("Final sum is: %lf\n", sum);
+        } else {
+            
+        printf("Current sum is: %lf\n", sum);
+            
+        }
+        
+        
+    }
+    
+    double average = sum / len;
+    printf("The average sum of the elemnts in the array is: %lf\n", average);
     return 0;
-}
+}   
--- ArraySum.c
+++ ArraySum.c
@@ -2,7 +2,7 @@
 * @Author: karlosiric
 * @Date:   2024-04-12 23:41:07
 * @Last Modified by:   karlosiric
-* @Last Modified time: 2024-04-13 00:42:09
+* @Last Modified time: 2024-04-13 15:26:10
 */
 
 #include <stdio.h>
@@ -13,17 +13,23 @@ int sum_array(int array[], int length);
 
 int main(void)
 {
-    int myArray[] = {1,2,6,9,8,7,6,5};
+    int myArray0[] = {1,2,6,9,8,7,6,5};
     int myArray2[] = {1,6,25,100,8,54,23,5};
     int myArray3[] = {1,43,124,120,8,54,6,90};
     // calculating 
-    int sum1 = sum_array(myArray, sizeof(myArray) / sizeof(myArray[0]));
+    
+    
+    int sum1 = sum_array(myArray0, sizeof(myArray0) / sizeof(myArray0[0]));
     int sum2 = sum_array(myArray2, sizeof(myArray2) / sizeof(myArray2[0]));
     int sum3 = sum_array(myArray3, sizeof(myArray3) / sizeof(myArray3[0]));
     
     printf("Sums of the arrays are %d\n", sum1);
     printf("Sums of the arrays are %d\n", sum2);
     printf("Sums of the arrays are %d\n", sum3);
+    
+    
+    
+      
     
     
     
